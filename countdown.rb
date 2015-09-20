e86ee15acf178100e8ca47933e2b6791cdcf4c55
wifelette <leahsilber@gmail.com>
2015-09-19 19:28:01 -0700

    So sloooowww

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(int)
+    while int > 0
+     puts "#{int} SECOND(S)!"
+     int -= 1
+    end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(int)
+    while int > 0
+      sleep 5
+      puts "#{int} SECOND(S)!"
+      int -= 1
+    end
+  return "HAPPY NEW YEAR!"
 end
